4bca813d3891be1b67cb449b73ea6358ba92519f
Mahtab Alam <smmalam9@gmail.com>
2017-10-18 23:19:40 +0530

    Python Practice

--- List/List.py
+++
@@ -1,8 +0,0 @@
-months = ['January','February','March']
-print('First Month: ', months[0])
-print('Second Month: ', months[1])
-print('Third Month: ', months[2])
-coords = [[1,2,3] , [4,5,6]]
-print(coords[0][0])
-print(coords[1][2])
-print(months[1][0])
---
+++ List/List2.py
@@ -0,0 +1,9 @@
+# write a program that prints out all the elements of the list that are less than 5.
+# print input in new list
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = []
+for element in a:
+    if element > 5:
+        b.append(element)
+print(b)
+print("\n")
