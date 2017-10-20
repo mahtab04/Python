3af6c817772ec2f75a9ae37ec9439700f2f86a75
Mahtab Alam <smmalam9@gmail.com>
2017-10-20 22:26:14 +0530

    Python Practice

--- DataType/DataType.py
+++ DataType/DataType.py
@@ -3,3 +3,16 @@ salary = input("Enter your salary ")
 bouns = input("Enter your bouns ")
 total = float(salary) + float(bouns)
 print("total bouns is", total)
+print("\n")
+x = 15
+print(int(x))
+print(float(x))
+print(complex(x))
+print("\n")
+n1 = 0o17    # octal no
+n2 = 0B1101010101  # binary number
+n3 = 0X1fb2c  # hexadecimal number
+print(int(n1))
+print(int(n2))
+print(int(n3))
+print("\n")
--- Turtle/Turtle.py
+++ Turtle/Turtle.py
@@ -1,23 +1,31 @@
 import turtle
 
-my_turtle = turtle.Turtle()
-my_turtle.forward(100)
-my_turtle.left(90)
-my_turtle.forward(100)
-my_turtle.left(90)
-my_turtle.forward(100)
-my_turtle.left(90)
-my_turtle.forward(100)
-my_turtle.left(90)
-my_turtle.right(100)
-my_turtle.forward(100)
-my_turtle.left(90)
-my_turtle.forward(100)
-my_turtle.left(90)
-my_turtle.forward(100)
-my_turtle.right(100)
-my_turtle.forward(100)
-my_turtle.left(100)
-my_turtle.forward(100)
-my_turtle.right(100)
- my_turtle.forward(500)
+#Import the turtle library
+import turtle
+
+#Declare your variables
+nbrSides = 0
+lengthOfLine = 20
+
+#Ask user how many sides they want for their object
+#Don't forget to convert that value into a integer
+#if you convert to float the for loop will give you an error because range values must be integers
+nbrSides = int(input("How many sides do you want on your object? " ))
+
+#Create a loop to draw the object
+for side in range(0,nbrSides) :
+    turtle.forward(lengthOfLine)
+    #the angle to turn depends on the number of sides of the object
+    turtle.right(360/nbrSides)
+
+    #This is the double bonus challenge, a nested loop that draws a smaller version of the object inside
+    for side in range(0,nbrSides) :
+        turtle.forward(lengthOfLine/2)
+        turtle.color('green')
+        turtle.right(360/nbrSides)
+        turtle.color('red')
+
+
+
+
+
