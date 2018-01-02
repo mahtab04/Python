acc7cda62ec17993e91fca756ededbb848e4ae3d
Mahtab Alam <smmalam9@gmail.com>
2018-01-02 13:28:46 +0530

    Python Practice

--- CombinationOfDigits/Combination.py
+++ CombinationOfDigits/Combination.py
@@ -2,12 +2,9 @@
 a = int(input("Enter first number:"))
 b = int(input("Enter second number:"))
 c = int(input("Enter third number:"))
-d = []
-d.append(a)
-d.append(b)
-d.append(c)
+d = [a, b, c]
 for i in range(0, 3):
     for j in range(0, 3):
         for k in range(0, 3):
-            if (i != j & j != k & k != i):
+            if i != j & j != k & k != i:
                 print(d[i], d[j], d[k])
